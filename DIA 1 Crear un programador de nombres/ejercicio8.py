3b4ea9539b40b47af35f17d8136b986b7c014221
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-05 20:56:19 -0300

    Función input

---
+++ DIA 1 Crear un programador de nombres/ejercicio8.py
@@ -0,0 +1,10 @@
+"""
+Práctica Input 2
+Crea un código que le permita ingresar una respuesta al usuario, 
+haciéndole la siguiente pregunta:
+
+¿En qué país vives?
+
+El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+"""
+print(input("¿En qué país vives?"))
---
+++ DIA 1 Crear un programador de nombres/ejercicio9.py
@@ -0,0 +1,15 @@
+"""
+Práctica Input 3
+Crea un código que muestre en pantalla el nombre completo del usuario,
+permitiéndole ingresar su nombre y apellido con las siguientes instrucciones:
+
+Escribe tu nombre:
+Escribe tu apellido:
+El código debe poder imprimir en pantalla el nombre y apellido del usuario, 
+separados por un espacio.
+"""
+# primer alternativa
+print(input("Escribe tu nombre:") + input("Escribe tu apellido:"))
+
+# segunda alternativa
+print(input("Escribe tu nombre: ") + " " + input("Escribe tu apellido: "))
