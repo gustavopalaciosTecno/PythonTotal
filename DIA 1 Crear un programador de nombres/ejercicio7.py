40fe3e36e9baa39d3925354456ec93dacf2c17b6
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-05 20:49:18 -0300

    Función input

---
+++ DIA 1 Crear un programador de nombres/ejercicio7.py
@@ -0,0 +1,19 @@
+"""
+Práctica Input 1
+Crea un código que le permita ingresar una respuesta al usuario, 
+haciéndole la siguiente pregunta:
+
+¿Qué estás estudiando?
+
+El código debe poder imprimir en pantalla lo ingresado por el usuario (utilizando print).
+
+Aclaración:
+
+1. No deben imprimirse strings adicionales a la respuesta del usuario.
+
+2. Los ejercicios que contienen el la función input() deben ser probados con el botón: 
+"Ejecutar pruebas".  Ya que el botón "Ejecutar código" arrojará el siguiente error: 
+"EOF when reading a line".
+"""
+
+print(input("¿Qué estás estudiando?"))
