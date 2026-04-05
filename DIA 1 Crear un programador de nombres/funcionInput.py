d66cc2b1472306ec0d727614af22318996f1da94
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-05 20:44:42 -0300

    Función input

---
+++ DIA 1 Crear un programador de nombres/funcionInput.py
@@ -0,0 +1,15 @@
+"""
+Usamos input para introducir datos y la función print 
+para mostrar esos datos en pantalla
+"""
+# print(input("Decime tu nombre: "))
+# print(input("Decime tu apellido: "))
+
+# Imprimir valores en una sola línea
+# print(input("Decime tu nombre: "),input("Decime tu apellido: "))
+
+# Imprimir en una sola pantalla concatenando valores
+print("tu nombre es: " + input("Decime tu nombre: ") + " y tu apellido es: " +  input("Decime tu apellido: "))
+
+
+
