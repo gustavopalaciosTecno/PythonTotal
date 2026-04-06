0e34b243536350d3760264caadaad7a821bd1201
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-05 21:42:57 -0300

    python

---
+++ DIA 2  - Programa una calcualdora de Comisiones/tipos_de_datos.py
@@ -0,0 +1,14 @@
+"""
+Existen varios tipos de datos en python:
+
+Cadenas(String)
+Enteros(Integer)
+Flotantes(Floating)
+Listas(lists)
+Diccionarios(Dictionary)
+Tuplas(Tuples)
+Conjuntos(Sets)
+Booleanos(Boolean)
+
+"""
+
