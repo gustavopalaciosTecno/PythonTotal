fdcd10bcb8a1136a18b3379c7e12625ab5a16f31
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-05 21:55:27 -0300

    Tipos de datos y variables

---
+++ DIA 2  - Programa una calcualdora de Comisiones/variables.py
@@ -0,0 +1,22 @@
+"""
+¿Qué es una variable?
+Imagina que tienes una caja donde guardas algo. 
+Le pones una etiqueta para saber qué hay adentro. 
+Eso es exactamente una variable: un nombre que apunta a un valor.
+"""
+# Ejemplo de variables
+nombre = "Néstor Gustavo"
+print(nombre)
+
+# para saber que tipo de datos maneja mi variable, agregar la función type
+print(type(nombre))
+
+numero = 44
+print(type(numero))
+
+sueldo = 1325.44
+print(type(sueldo))
+
+apellidosEspañoles = ["Gonzalez","Fernandez","Marquez","Vazquez"]
+print(type(apellidosEspañoles))
+
