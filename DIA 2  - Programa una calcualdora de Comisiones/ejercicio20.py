bf843d3d754549f91ea468ae2e655a0648a09d13
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-07 08:47:30 -0300

    formatear cadenas

---
+++ DIA 2  - Programa una calcualdora de Comisiones/ejercicio20.py
@@ -0,0 +1,9 @@
+"""
+Práctica Formatear Cadenas 2
+Muestra al usuario la cantidad de puntos acumulados dentro de la siguiente frase:
+
+Has ganado (puntos_nuevos) puntos! En total, acumulas (puntos_totales) puntos
+"""
+puntos_nuevos = 350
+puntos_totales = 1225
+print(f"Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos")
