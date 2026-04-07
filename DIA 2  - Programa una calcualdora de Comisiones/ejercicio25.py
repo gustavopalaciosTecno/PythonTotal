a8a7bbef330f44499c1c88a6c73293742b4b5498
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-07 09:16:26 -0300

    Redondeo

---
+++ DIA 2  - Programa una calcualdora de Comisiones/ejercicio25.py
@@ -0,0 +1,7 @@
+"""
+Práctica Redondeo 1
+Redondea el resultado de la división 10/3 a un número con 2 decimales,
+y muestra en pantalla el valor redondeado.
+"""
+division = 10/3
+print(round(division,2))
