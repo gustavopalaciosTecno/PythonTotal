6214d4acd2020ec7c189f5d47ae2ce461dbab101
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-04-07 08:46:14 -0300

    Formatear cadenas

---
+++ DIA 2  - Programa una calcualdora de Comisiones/formateo_cadenas.py
@@ -0,0 +1,11 @@
+nombre = "Leticia"
+apellido = "Bredice"
+
+# Cadenas literales - PRIMERA FORMA TE MOSTRAR LOS VALORES - esta es la mejor forma
+print(f"mi nombre es: {nombre} y mi apellido es: {apellido}") 
+
+# Función format - SEGUNDA FORMA DE MOSTRAR LOS VALORES
+print("mi nombre es {} y mi apellido es: {}".format(nombre,apellido))
+
+# Otra forma alternativa pero no muy efectiva
+print("mi nombre es: ", nombre + " y mi apellido es: ", apellido)
