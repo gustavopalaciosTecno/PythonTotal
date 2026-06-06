6b0bc508fb6b9f3dac4c5f65eae28b7dd8ecfe62
gustavo <[200~110218589+gustavopalaciosTecno@users.noreply.github.com~>
2026-06-06 10:40:02 -0300

    Strings

--- DIA 3 - Prorgrama un analizador de Texto/Propiedaes_string.py
+++ DIA 3 - Prorgrama un analizador de Texto/Propiedaes_string.py
@@ -3,6 +3,12 @@ Los strings:
 Son inmutables
 Pueden concatenarse
 Son multiplicables
-
+Son multilineales
+Puede verificarse si un sibstring se encuentra
+dentro de su contenido
+Puede calcular el lago de un string
 """
+
+print(15 * "Repetición")
+
 
